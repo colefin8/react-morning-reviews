87be6cccebcb2cd2aca0fc766d215a390489e674
Cole Finlayson <cole.fin8@gmail.com>
2020-10-21 09:54:07 -0600

    day 1

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+import React, { Component } from "react";
+import List from './components/List'
+import "./App.css";
 
+// class App extends Component {
+//   constructor() {
+//     super();
+//   }
+
+//   render() {
+//     return (
+//       <div className="App" id='my-id'>
+//         <h1 className="movie-list-title">The Most Amazing Movie List</h1>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+
+  return (
+    <div className="App" id="my-id">
+      <h1 className="movie-list-title">The Most Amazing Movie List</h1>
+      
+      <List/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
+
+  render() {
+    return <div>List Component</div>;
+  }
+}
+
+export default List;
